d2f69c90816c8dbb103334154ca277970483ece4
artichokee <samanthaatringali@gmail.com>
2022-07-04 14:00:13 -0400

    Isolated recipe name text

--- MonbentoScrape.py
+++ MonbentoScrape.py
@@ -3,17 +3,16 @@ import requests
 import bs4
 
 base_url = input("Enter recipe URL: ")
-
 res = requests.get(base_url)
-
 soup = bs4.BeautifulSoup(res.text, "lxml")
 
 # isolate section with name and recipe details
 recipe = soup.select('.penci-recipe-metades')
 header = recipe[0]
+#print(f"{header}")
 
-print(f"{header}")
 
-recipe_name = header.find("span", itemprop="name")
-
+recipe_name_soup = soup.find('h2', {'itemprop': 'name'})
+recipe_name = recipe_name_soup.text
 print(f"{recipe_name}")
+
